f75bd02f8f5cb5d59d3923a71d4c684fa9793eed
vitalyandreev2 <vitaly.andreev2@icl-services.com>
2022-03-31 21:27:56 +0700

    lesson-1

---
+++ resources/views/index.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Hello, <?=$user?></h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Admin\NewsController as AdminNewsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/hello', function () {
     return view('hello');
@@ -25,6 +27,11 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/news', function () {
-    return view('news');
+Route::get('/news', [NewsController::class, 'index'])->name('news');
+Route::get('/news/category{id}', [NewsController::class, 'category'])->where('id', '\d+')->name('news.category');
+Route::get('/news/category{idCategory}/news{id}', [NewsController::class, 'show'])->where(['idCategory' => '\d+', 'id' => '\d+'])->name('news.show');
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function() {
+    Route::resource('categories', AdminCategoryController::class);
+    Route::resource('news', AdminNewsController::class); 
 });
